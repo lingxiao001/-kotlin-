8e481b55e953737c5645eac245376e4e91d90a1d
lingxiao001 <lingxiao98899@gmail.com>
2025-05-29 15:50:06 +0800

    fix test file

--- src/lackofindentifier.c
+++ src/lackofindentifier.c
@@ -1,1 +1,11 @@
-int 666;
+
+int main(){
+    int = 10;// 缺少标识符identifier :x
+    float y = 3.14;
+
+    if (x > 5) {
+        printf("Hello World\n");
+    }
+
+    return 0;
+}
--- src/lackof{.c
+++ src/lackof{.c
@@ -1,1 +1,11 @@
-int foo(haha);
+
+int main()
+    int x = 10;
+    float y = 3.14;
+
+    if (x > 5) {
+        printf("Hello World\n");
+    }
+
+    return 0;
+}
